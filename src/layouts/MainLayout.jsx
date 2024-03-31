053861d748d24079ccc1c93da5396fb1b215f875
Syed Zeeshan Ahmed <s.zeeshan47@gmail.com>
2024-03-31 08:55:36 -0400

    pages created, job listing created, dynamic visibility done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Outlet} from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+   <>
+   <Navbar />
+   <Outlet />
+   </>
   )
 }
 
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import JobListings from '../components/JobListings'
+
+const JobsPage = () => {
+  return (
+    <>
+    <section className='bg-blue50 px-4 py-6'>
+      <JobListings />
+    </section>
+    </>
+  )
+}
+
+export default JobsPage
